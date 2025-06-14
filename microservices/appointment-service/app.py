dd7ea8e9f1eba34ea5d0a453d130fa0181100ee2
Sumanthreddypmr <sumanthkumardev95@gmail.com>
2025-06-14 07:45:00 +0000

    updated with v1

--- microservices/appointment-service/app.py
+++ microservices/appointment-service/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Appointment Service!"
+    return "Hello from Appointment Service! v1111"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
